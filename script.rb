89791cef62863568233a60de5c4d638ddd4081f5
Hsin Yi Huang <151099887+nekohamuchan@users.noreply.github.com>
2024-10-24 13:43:22 +0800

    Add main codes

--- script.rb
+++ script.rb
@@ -1,3 +1,17 @@
-def substrings(word, arr)
-
+def substrings(str, dictionary)
+  list = Hash.new(0)
+  test = str.downcase
+  for word in dictionary
+    if test.include?(word)
+      list[word] = test.scan(word).length
+    end
+  end
+  list
 end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+puts "String: below"
+puts substrings("below", dictionary)
+puts
+puts "String: Howdy partner, sit down! How's it going?"
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
